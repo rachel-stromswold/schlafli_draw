322a255da139a5ffd10c71e12ed3b8a27e5362b2
Devon Loehr <devon.loehr@gmail.com>
2014-04-14 23:25:35 -0400

    Heavy stylistic modifications to the button class, but no functional changes

--- gui/button.cpp
+++ gui/button.cpp
@@ -4,62 +4,84 @@
 #include "diagram.h"
 #include <iostream>
 
-int toInt(std::string str){
-    int ret=0;
-    for(int i=str.length()-1;i>=0;i--){
-        ret+=pow(10,str.length()-1-i)*(str[i]-'0');
+Button::Button(sf::RenderWindow* window, sf::Font font, int x, int y, int width, int height, std::string str) :
+        m_text(),
+        m_rectangle(sf::Vector2f(width * 15, height * 15)) ,
+        m_f(font)
+{
+        m_w = window;
+        m_x = x;
+        m_y = y;
+        m_width = width;
+        m_height = height;
+
+        m_text.setString(str);
+        m_text.setFont(m_f);
+        m_text.setCharacterSize(15);
+        m_text.setColor(sf::Color::Black);
+        m_rectangle.setOutlineThickness(2);
+        m_rectangle.setOutlineColor(sf::Color(255,255,255));
+
+        m_rectangle.setPosition(x,y);
+        m_text.setPosition(x,y-2);
+    }
+
+int toInt(std::string str) {
+    int ret = 0;
+    for(int i = str.length() - 1; i >= 0; i--) {
+        ret += pow(10, str.length() - 1 - i)*(str[i] - '0');
     }
     return ret;
 }
 
-sf::VertexArray Button::getDiagram(std::string str){
-    int p=0;int q=1;
-    if(str.find('{')!=std::string::npos && str.rfind('}')!=std::string::npos){//if both { and } are present
-        str=str.substr(str.find('{')+1,str.rfind('}')-str.find('{')-1);
+sf::VertexArray Button::getDiagram(std::string str) {
+    int p = 0;
+    int q = 1;
+    if(str.find('{') != std::string::npos && str.rfind('}') != std::string::npos) {//if both { and } are present
+        str = str.substr(str.find('{') + 1, str.rfind('}') - str.find('{') - 1);
     }
 
-    if(str.find('/')!=std::string::npos){
-        p=toInt(str.substr(0,str.find('/')));
-        q=toInt(str.substr(str.find('/')+1,std::string::npos));
-    }else{
-        p=toInt(str);
-        q=1;
+    if(str.find('/')!=std::string::npos) {
+        p = toInt(str.substr(0, str.find('/')));
+        q = toInt(str.substr(str.find('/') + 1, std::string::npos));
+    } else {
+        p = toInt(str);
+        q = 1;
     }
 
     sf::VertexArray ret(sf::Lines,p);
 
-    int currAngle=0;
-    int nextAngle=0;
-    int scale=64;
-    int offset=100;
+    int currAngle = 0;
+    int nextAngle = 0;
+    int scale = 64;
+    int offset = 100;
 
-    for(int i=0;i<ret.getVertexCount();i++){
-        //std::cout<<i*6.283/p<<std::endl;
-        //std::cout<<(i+q)%p*6.283/p<<std::endl;
+    for(int i = 0; i < ret.getVertexCount(); i++) {
+        //std::cout << i * 6.283 / p << std::endl;
+        //std::cout << (i + q) % p * 6.283 / p << std::endl;
 
         //calculates the angle that each given point will be at
-        currAngle=i*6.283/p;
-        nextAngle=(i+q)%p*6.283/p;
+        currAngle = i * 6.283 / p;
+        nextAngle = (i + q) % p * 6.283 / p;
 
         //Actually adds in the lines to the diagram
-        ret[i].position=sf::Vector2f(cos(currAngle)*scale+offset,sin(currAngle)*scale+offset);
-        ret[i+1].position=sf::Vector2f(cos(nextAngle)*scale+offset,sin(nextAngle)*scale+offset);
-        ret[i].color=sf::Color::White;
-        ret[i+1].color=sf::Color::White;
+        ret[i].position = sf::Vector2f(cos(currAngle) * scale + offset, sin(currAngle) * scale + offset);
+        ret[i + 1].position = sf::Vector2f(cos(nextAngle) * scale + offset, sin(nextAngle) * scale + offset);
+        ret[i].color = sf::Color::White;
+        ret[i + 1].color = sf::Color::White;
     }
-
     return ret;
 }
 
 void Button::draw(){
-    w->draw(rectangle);
-    w->draw(text);
-    w->draw(shape);
+    m_w->draw(m_rectangle);
+    m_w->draw(m_text);
+    m_w->draw(m_shape);
 }
 
 void Button::press(int xP, int yP, InputBox b){
-    if(x<xP && xP<x+width*15 && y<yP && yP<y+height*15){
-        //d=Diagram(w,0,0,100,100,b.stored.getString());
-        shape=getDiagram(b.stored.getString());
+    if(m_x < xP && xP <m_x + m_width * 15 && m_y < yP && yP < m_y + m_height * 15) {
+        //d = Diagram(w, 0, 0, 100, 100, b.stored.getString());
+        m_shape = getDiagram(b.stored.getString());
     }
 }
--- gui/button.h
+++ gui/button.h
@@ -1,50 +1,36 @@
+#ifndef BUTTON_H
+#define BUTTON_H
+
 #include <SFML/Graphics.hpp>
 #include <string>
 #include "text.h"
 #include "diagram.h"
-#ifndef H_BUTTON
-#define H_BUTTON
 
 class Button{
-public:
-    int x;
-    int y;
-    int width;
-    int height;
-    //Diagram d;
-
-    sf::Text text;
-    sf::Font f;
-
-    void draw();
-    void press(int xP,int yP,InputBox b);//only activates if the button was actually at the x and y location
-    //void activate
-
-    Button(sf::RenderWindow* myWindow, sf::Font font,int myX,int myY,int myWidth,int myHeight,std::string str) : text(),rectangle(sf::Vector2f(myWidth*15, myHeight*15)),f(font){
-        w=myWindow;
-        x=myX;
-        y=myY;
-        width=myWidth;
-        height=myHeight;
-
-        text.setString(str);
-        text.setFont(f);
-        text.setCharacterSize(15);
-        text.setColor(sf::Color::Black);
-        rectangle.setOutlineThickness(2);
-        rectangle.setOutlineColor(sf::Color(255,255,255));
-
-        rectangle.setPosition(x,y);
-        text.setPosition(x,y-2);
-    };
 private:
-    sf::RenderWindow* w;
-    sf::RectangleShape rectangle;
+    int m_x;
+    int m_y;
+    int m_width;
+    int m_height;
+    //Diagram m_d;
 
-    sf::VertexArray shape;
+    sf::Text m_text;
+    sf::Font m_f;
+
+    sf::RenderWindow* m_w;
+    sf::RectangleShape m_rectangle;
+
+    sf::VertexArray m_shape;
 
     //Returns a Vertex Array of the shape that can be drawn to the window
     sf::VertexArray getDiagram(std::string str);
+
+public:
+    void draw();
+    void press(int xP,int yP,InputBox b); //only activates if the button was actually at the x and y location
+    //void activate
+
+    Button(sf::RenderWindow* window, sf::Font font, int x, int y, int width, int height, std::string str);
 };
 
-#endif //H_BUTTON
+#endif //BUTTON_H
